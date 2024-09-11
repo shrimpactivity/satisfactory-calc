84876b653880fde3e2fda6ce356b1756218fbb7c
Carson Crow <carson.crow@texasbar.com>
2024-09-11 15:47:53 -0500

    finished factory

--- src/factory.py
+++ src/factory.py
@@ -1,23 +1,48 @@
-RAW_MATERIALS = ["iron_ore", "copper_ore", "limestone"]
+# Get production/consumption rate (per minute) based on recipe duration and item amount
+def getRate(amount, duration, rounding=1):
+    return round(60 / duration, rounding) * amount
 
 # Generates a tree encoded as list [item, rate, [children]]
-def factoryTree(item, rate, item_recipes):
-    byproducts = {}
+def buildFactory(item, rate, item_recipes, raw_materials):
+    byproduct_totals = {}
+    raw_material_totals = {}
 
-    def factoryTreeHelper(item, rate, item_recipes):
+    def buildFactoryHelper(item, rate):
         # Base case
         if item not in item_recipes:
-            if item not in RAW_MATERIALS:
+            if item not in raw_materials:
                 print(f"No recipe available for {item}")
+            if item in raw_material_totals:
+                raw_material_totals[item] += rate
+            else:
+                raw_material_totals[item] = rate
             return [item, rate, []]
         # Base case in the event of looping recipe?
         if rate < 0.01:
             return [item, 0, []]
 
-        recipe = item_recipes["item"]
-        relevant_product = [p["item"] for p in recipe["products"] if p["item"] == item]
-        byproducts = [p["item"] for p in recipe["products"] if p["item"] != item]
-        item_rate = round(60 / recipe["duration"], 2)
-        ingredient_items = [ingredient["item"] for ingredient in recipe["ingredients"]]
-        return [item, rate, ]
+        recipe = item_recipes[item]
+        product = [p for p in recipe["products"] if p["item"] == item][0]
+        multiplier = round(rate / getRate(product["amount"], recipe["duration"], 5), 2)
+
+        # Track byproducts and include in result
+        byproducts = [p for p in recipe["products"] if p["item"] != item]
+        for product in byproducts:
+            byproduct_item = product["item"]
+            byproduct_rate = getRate(product["amount"], recipe["duration"]) * multiplier
+            if byproduct_item in byproduct_totals:
+                byproduct_totals[byproduct_item] += byproduct_rate
+            else:
+                byproduct_totals[byproduct_item] = byproduct_rate
+        
+        children = []
+        for ingredient in recipe["ingredients"]:
+            consumption_rate = getRate(ingredient["amount"], recipe["duration"]) * multiplier
+            children.append(buildFactoryHelper(ingredient["item"], consumption_rate))
+        return [item, rate, children]
     
+    return {
+        "tree": buildFactoryHelper(item, rate), 
+        "byproducts": byproduct_totals,
+        "raw_materials": raw_material_totals
+    }
--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import factory
 import recipe
 
+# Use specified alternate recipes
 USE_RECIPES = []
+# Items with no recipes
 RAW_MATERIALS = ["iron_ore", "copper_ore", "limestone"]
-product = []
+# Item to produce
+ITEM = "smart_plating"
+# Rate to produce item (per minute)
+RATE = 120
 
 def main():
     recipes = recipe.getRecipes(USE_RECIPES)
-    print(recipes["rotor"])
+    print(factory.buildFactory(ITEM, RATE, recipes, RAW_MATERIALS))
 
 if __name__ == "__main__":
     main()
