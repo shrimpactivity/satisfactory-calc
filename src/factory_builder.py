560bf6b36003f55518bccf5ae69c67f0bcb1fd49
Carson Crow <carsoncrow@pm.me>
2024-09-11 22:27:59 -0500

    added machine count

--- src/factory_builder.py
+++ src/factory_builder.py
@@ -1,3 +1,5 @@
+import math
+
 # Get production/consumption rate (per minute) based on recipe duration and item amount
 def getRate(amount, duration):
     return 60 / duration * amount
@@ -5,35 +7,40 @@ def getRate(amount, duration):
 # Generates a tree encoded as list [item, rate, [children]]
 def buildFactory(items, rates, item_recipes):
     ingredient_totals = {}
+    machine_totals = {}
     byproduct_totals = {}
 
     def buildFactoryHelper(item, rate):
         # Base case
         if item not in item_recipes:
-            return [item, rate, []]
+            return [item, rate, 0, []]
         # Base case in the event of looping recipe?
         if rate < 0.01:
             return [item, 0, []]
 
         recipe = item_recipes[item]
-        product = [p for p in recipe["products"] if p["item"] == item][0]
-        multiplier = rate / getRate(product["amount"], recipe["duration"])
+        recipe_product = [p for p in recipe["products"] if p["item"] == item][0]
+        machines = rate / getRate(recipe_product["amount"], recipe["duration"])
+        if item in machine_totals:
+            machine_totals[item] += machines
+        else:
+            machine_totals[item] = machines
 
         # Track byproducts and include in result
         byproducts = [p for p in recipe["products"] if p["item"] != item]
         for product in byproducts:
             byproduct_item = product["item"]
-            byproduct_rate = getRate(product["amount"], recipe["duration"]) * multiplier
+            byproduct_rate = getRate(product["amount"], recipe["duration"]) * machines
             byproduct_rate = round(byproduct_rate, 1)
             if byproduct_item in byproduct_totals:
                 byproduct_totals[byproduct_item] += byproduct_rate
             else:
                 byproduct_totals[byproduct_item] = byproduct_rate
-        
+
         children = []
         for ingredient in recipe["ingredients"]:
             ingredient_item = ingredient["item"]
-            consumption_rate = getRate(ingredient["amount"], recipe["duration"]) * multiplier
+            consumption_rate = getRate(ingredient["amount"], recipe["duration"]) * machines
             consumption_rate = round(consumption_rate, 1)
             
             # Add to total ingredient count
@@ -43,13 +50,14 @@ def buildFactory(items, rates, item_recipes):
                 ingredient_totals[ingredient_item] = consumption_rate
             
             children.append(buildFactoryHelper(ingredient_item, consumption_rate))
-        return [item, round(rate, 2), children]
+        return [item, round(rate, 2), math.floor(machines + 1), children]
     
     trees = [buildFactoryHelper(items[i], rates[i]) for i in range(len(items))]
 
     return {
         "trees": trees,
         "ingredient_totals": ingredient_totals,
+        "machine_totals": machine_totals,
         "byproducts": byproduct_totals,
     }
 
@@ -59,8 +67,9 @@ def scaleFactory(factory, scale):
             return tree
         item = tree[0]
         amount = round(tree[1] * scale, 2)
+        machines = round(tree[1] * scale, 2)
         children = [scaleFactoryTree(t) for t in tree[2]]
-        return [item, amount, children]
+        return [item, amount, machines, children]
     trees = [scaleFactoryTree(t) for t in factory['trees']]
     ingredient_totals = factory["ingredient_totals"]
     for ing in ingredient_totals:
@@ -76,7 +85,7 @@ def scaleFactoryToIngredient(ingredient, amount, factory):
     scale = amount / factory["ingredient_totals"][ingredient]
     scaleFactory(factory, scale)
 
-RAW_MATERIALS = ["iron_ore", "copper_ore", "limestone", "coal"]
+RAW_MATERIALS = ["iron_ore", "copper_ore", "limestone", "coal", "sulfur", "quartz"]
 
 def prettyPrintFactory(factory, tabSize=4):
 
@@ -86,11 +95,13 @@ def prettyPrintFactory(factory, tabSize=4):
         
         item = tree[0]
         rate = tree[1]
-        children = tree[2]
+        machines = tree[2]
+        children = tree[3]
 
         whitespace = " " * (tabs) * tabSize
         whitespace += "\u25D9" if tabs == 0 else "\u21B3"
-        print(f" {whitespace} {item} - {rate}")
+        machines_text = "" if machines == 0 else f"({machines} machines)"
+        print(f" {whitespace} {item} - {rate} {machines_text}")
         for child in children:
             prettyPrintTreeHelper(child, tabs + 1)
     
@@ -101,7 +112,9 @@ def prettyPrintFactory(factory, tabSize=4):
     print("\n=== INGREDIENT TOTALS ===")
     for item in factory['ingredient_totals']:
         rate = factory['ingredient_totals'][item]
-        print(f"\u2022 {item} - {rate}")
+        machines = 0 if item not in factory['machine_totals'] else factory['machine_totals'][item]
+        machines_text = "" if machines == 0 else f"({machines} machines)"
+        print(f"\u2022 {item} - {rate} {machines_text}")
     print("\n=== RAW MATERIALS ===")
     for item in [x for x in factory['ingredient_totals'] if x in RAW_MATERIALS]:
         rate = factory['ingredient_totals'][item]
--- src/main.py
+++ src/main.py
@@ -4,15 +4,17 @@ import recipe
 # Use specified alternate recipes
 USE_RECIPES = []
 # Item to produce
-ITEMS = ["smart_plating"]
+ITEMS = ["modular_frame"]
 # Rate to produce item (per minute)
 RATES = [5]
 
 def main():
     recipes = recipe.getRecipes(USE_RECIPES)
     factory = factory_builder.buildFactory(ITEMS, RATES, recipes)
-    #factory_builder.scaleFactoryToIngredient("iron_plate", 180, factory)
+    #factory_builder.scaleFactoryToIngredient("iron_ore", 150, factory)
     factory_builder.prettyPrintFactory(factory)
 
 if __name__ == "__main__":
     main()
+
+# TODO: list number of machines with ingredient list
