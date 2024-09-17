c3018c1f2ef94c803e24a7a4546867f9ce7f6a01
Carson Crow <carsoncrow@pm.me>
2024-09-17 17:15:06 -0500

    .

--- src/config.py
+++ src/config.py
@@ -1,30 +1,19 @@
-template = {
-    "factory": {},
-    "recipes": [],
-    "ingredient_supply": {}
+phase_2 = {
+    "smart_plating": 1000 / 180,
+    "versatile_framework": 1000 / 180,
+    "automated_wiring": 100 / 180,
+    "steel_beam": 200 / 30,
+    "steel_pipe": 200 / 30,
+    "encased_industrial_beam": 50 / 30,
+    "cable": 500 / 30,
+    "modular_frame": 100 / 30,
+    "concrete": 1000 / 30,
+    "rotor": 200 / 30,
+    "reinforced_iron_plate": 400 / 30,
+    "copper_sheet": 500 / 30,
+    "wire": 2000 / 30,
 }
 
-phase_2 = {
-    "factory": {
-        "smart_plating": 1000 / 180,
-        "versatile_framework": 1000 / 180,
-        "automated_wiring": 100 / 180,
-        "steel_beam": 200 / 30,
-        "steel_pipe": 200 / 30,
-        "encased_industrial_beam": 50 / 30,
-        "cable": 500 / 30,
-        "modular_frame": 100 / 30,
-        "concrete": 1000 / 30,
-        "rotor": 200 / 30,
-        "reinforced_iron_plate": 400 / 30,
-        "copper_sheet": 500 / 30,
-        "wire": 2000 / 30,
-    },
-    "recipes": [
-        "encased_industrial_pipe"
-    ],
-    "ingredient_supply": {}
-}
 
 # Item to produce and their rate (per min.)
 ITEMS_AND_RATES = {
@@ -35,4 +24,5 @@ USE_RECIPES = ["encased_industrial_pipe"]
 # Scale factory based on ingredient availability
 INGREDIENT_SUPPLY = {}
 
-RAW_MATERIALS = ["iron_ore", "copper_ore", "limestone", "coal", "sulfur", "raw_quartz", "sam", "crude_oil"]
+RAW_MATERIALS = ["iron_ore", "copper_ore", "limestone",
+                 "coal", "sulfur", "raw_quartz", "sam", "crude_oil"]
